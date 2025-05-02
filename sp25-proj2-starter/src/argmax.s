32669c2237281e079477f738fed1ac6456730fab
Hector <hectorgetfree@gmail.com>
2025-05-02 16:46:02 +0800

    completed proj2-task3

--- sp25-proj2-starter/src/argmax.s
+++ sp25-proj2-starter/src/argmax.s
@@ -16,15 +16,33 @@
 # =================================================================
 argmax:
     # Prologue
+    li t0, 1
+    blt a1, t0, exit
 
-
+    # 复制数组起始地址
+    addi t5, a0, 0
+    # 设置初始索引
+    lw t6, 0(a0)
+    li a0, 0
 loop_start:
-
-
+    bge t0, a1, loop_end
+    # 设置偏移量
+    slli t2, t0, 2
+    # 设置地址
+    add t3, t5, t2
+    # 储存值
+    lw t4, 0(t3)
+    # 进行比较
+    bge t6, t4, loop_continue
+    # 更新最大索引
+    mv t6, t4
+    mv a0, t0
 loop_continue:
-
-
+    addi t0, t0, 1
+    jal x0, loop_start
 loop_end:
     # Epilogue
-
     jr ra
+exit:
+    li a0, 36
+    ecall
