4414d6b00f70ec29db51e85d3eff5afae84ccf61
Hector <hectorgetfree@gmail.com>
2025-05-02 15:29:14 +0800

    complete proj2-task1

--- sp25-proj2-starter/src/abs.s
+++ sp25-proj2-starter/src/abs.s
@@ -7,12 +7,22 @@
 #   a0 (int*) is a pointer to the input integer
 # Returns:
 #   None
-# =================================================================
+# ================================================================
+ebreak
 abs:
     # Prologue
 
-    # PASTE HERE
+    # Load number from memory
+    lw t0 0(a0)
+    bge t0, zero, done
+
+    # Negate a0
+    sub t0, x0, t0
+
+    # Store number back to memory
+    sw t0 0(a0)
+
+done:
+    jr ra
 
     # Epilogue
-
-    jr ra
