a760f1c13056d34fe95d744d1c06831150e7e989
Hector <hectorgetfree@gmail.com>
2025-05-05 15:41:56 +0800

    Attempting to access uninitialized memory between the stack and heap. Attempting to access '4' bytes at address '0x10008150'.

--- sp25-proj2-starter/src/classify.s
+++ sp25-proj2-starter/src/classify.s
@@ -24,7 +24,7 @@
 # Usage:
 #   main.s <M0_PATH> <M1_PATH> <INPUT_PATH> <OUTPUT_PATH>
 classify:
-    addi sp, sp, -52
+    addi sp, sp, -48
     sw s0, 0(sp)
     sw s1, 4(sp)
     sw s2, 8(sp)
@@ -36,8 +36,7 @@ classify:
     sw s8, 32(sp)
     sw s9, 36(sp)
     sw s10, 40(sp)
-    sw s11, 44(sp)
-    sw ra, 48(sp)
+    sw ra, 44(sp)
 
     li t0, 5
     bne a0, t0, exit_arg
@@ -51,91 +50,97 @@ classify:
     jal malloc
     li t0, 0
     beq a0, t0, exit_mal
-    mv t0, a0
-    lw a0, 4(s0)
-    lw a1, 0(t0)
-    lw a2, 4(t0)
-    jal read_matrix
     mv s2, a0
-    mv s3, a1
-    mv s4, a2
+    lw a0, 4(s0)
+    addi a1, s2, 0
+    addi a2, s2, 4
+    jal read_matrix
+    mv s3, a0
+    # do not try to use the a register directly after the function call
     # Read pretrained m1
     li a0, 8
     jal malloc
     li t0, 0
     beq a0, t0, exit_mal
-    mv t0, a0
+    mv s4, a0
     lw a0, 8(s0)
-    lw a1, 0(t0)
-    lw a2, 4(t0)
+    # want to get the address
+    # don't try to lw
+    # but using offset
+    addi a1, s4, 0
+    addi a2, s4, 4
     jal read_matrix
     mv s5, a0
-    mv s6, a1
-    mv s7, a2
     # Read input matrix
     li a0, 8
     jal malloc
     li t0, 0
     beq a0, t0, exit_mal
-    mv t0, a0
+    mv s6, a0
     lw a0, 12(s0)
-    lw a1, 0(t0)
-    lw a2, 4(t0)
+    addi a1, s6, 0
+    addi a2, s6, 4
     jal read_matrix
-    mv s8, a0
-    mv s9, a1
-    mv s10, a2
+    mv s7, a0
     # Compute h = matmul(m0, input)
     # malloc space for h
-    mul a0, s3, s10
+    lw t0, 0(s2)
+    lw t1, 4(s6)
+    mul a0, t0, t1
+    slli a0, a0, 2
     jal malloc
     li t0, 0
     beq a0, t0, exit_mal
-    mv s11, a0
+    mv s8, a0
 
-    mv a0, s2
-    mv a1, s3
-    mv a2, s4
-    mv a3, s5
-    mv a4, s4
-    mv a5, s10
-    mv a6, s11
+    mv a0, s3
+    lw a1, 0(s2)
+    lw a2, 4(s2)
+    mv a3, s7
+    lw a4, 0(s6)
+    lw a5, 4(s6)
+    mv a6, s8
     jal matmul
     # Compute h = relu(h)
-    mv a0, s11
-    mul a1, s3, s4
+    lw t0, 0(s2)
+    lw t1, 4(s6)
+    mul a1, t0, t1
     jal relu
     # Compute o = matmul(m1, h)
-    mul a0, s6, s10
+    lw t0, 0(s4)
+    lw t1, 4(s6)
+    mul a0, t0, t1
     jal malloc
     li t0 0
     beq a0, t0, exit_mal
     mv s9, a0
 
     mv a0, s5
-    mv a1, s6
-    mv a2, s7
-    mv a3, s11
-    mv a4, s3
-    mv a5, s10
+    lw a1, 0(s4)
+    lw a2, 4(s4)
+    mv a3, s8
+    lw a4, 0(s2)
+    lw a5, 4(s6)
     mv a6, s9
     jal matmul
 
     # Write output matrix o
-    lw a0, 16(s1)
+    lw a0, 16(s0)
     mv a1, s9
-    mv a2, s6
-    mv a3, s10
+    lw a2, 0(s4)
+    lw a3, 4(s6)
     jal write_matrix
     # Compute and return argmax(o)
     mv a0, s9
-    mul a1, s6, s10
+    lw t0, 0(s4)
+    lw t1, 4(s6)
+    mul a1, t0, t1
     jal argmax
-    mv s0, a0
+    mv s10, a0
     # If enabled, print argmax(o) and newline
-    li t0, 1
+    li t0, 0
     bne s1, t0, exit_pri
-    mv a0, s0
+    mv a0, s10
     jal print_int
 
     li a0, '\n'
@@ -159,14 +164,10 @@ exit_pri:
     jal free
     mv a0, s9
     jal free
-    mv a0, s10
-    jal free
-    mv a0, s11
-    jal free
-    mv a0, s0
 
-    lw ra, 48(sp)
-    lw s11, 44(sp)
+    mv a0, s10
+
+    lw ra, 44(sp)
     lw s10, 40(sp)
     lw s9, 36(sp)
     lw s8, 32(sp)
@@ -178,7 +179,7 @@ exit_pri:
     lw s2, 8(sp)
     lw s1, 4(sp)
     lw s0, 0(sp)
-    addi sp, sp, 52
+    addi sp, sp, 48
     jr ra
 
 exit_arg:
